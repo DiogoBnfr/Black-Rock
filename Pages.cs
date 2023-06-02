cf7ad4bc43df8f0916ad9fd6e9dab177402d1997
diogobnfr <diogobonofre@gmail.com>
2023-06-02 17:43:54 -0300

    Added bunker manager page Prompt and error interfaces Setup abstraction method

--- Pages.cs
+++ Pages.cs
@@ -1,12 +1,23 @@
+using Console = System.Console;
+
 namespace BlackRock;
 
 public static class Pages
 {
-    public static void BunkerManager()
+    public static int BunkerManager()
     {
-        Interface.Center("BLACK ROCK", ':');
-        FileHandler.ScanRoot();
-        Interface.Center("| 1 : Open bunker | 2 : Create bunker | 3 : Delete bunker |", ':');
+        int input;
+        while (true)
+        {
+            Interface.Center("BLACK ROCK", ':');
+            FileHandler.ScanRoot();
+            Interface.Center("| 1 : Open bunker | 2 : Create bunker | 3 : Delete bunker |", ':');
+            
+            int[] options = { 1, 2, 3 };
+            input = Interface.Prompt(options);
+            if (input != -1) break; 
+        }
+        return input;
     }
 
     public static void DepositManager()
@@ -31,5 +42,48 @@ public static class Interface
         for (int i = 0; i < padding; i++) Console.Write(symbol);
         
         Console.WriteLine();
+    }
+
+    public static int Prompt(int[] options)
+    {
+        Console.Write("Insert an option: ");
+
+        try
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int option in options)
+            {
+                if (input == option)
+                {
+                    return input;
+                }
+            }
+
+            Error("Invalid input! Please try again.");
+        }
+        catch (FormatException)
+        {
+            Error("Invalid type! Please insert an integer.");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Error("Invalid input! Please insert an existent option.");
+        }
+        catch (OverflowException)
+        {
+            Error("Invalid input! ...");
+        }
+        return -1;
+    }
+
+    private static void Error(string exception)
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(); Interface.Center(exception, ' '); Console.WriteLine();
+        Thread.Sleep(1000);
+        Console.ResetColor();
+        Console.Clear();
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,10 +4,17 @@ internal static class Program
 {
     public static void Main(string[] args)
     {
-        Environment.CurrentDirectory = @"C:\Users\" + Environment.UserName + @"\Documents";
-        if (!Directory.Exists(Environment.CurrentDirectory + @"\BlackRock")) FileHandler.CreateRoot();
-        Environment.CurrentDirectory += @"\BlackRock";
-        
+        Setup(); 
         Pages.BunkerManager();
+    }
+
+    private static void Setup()
+    {
+        Environment.CurrentDirectory = @"C:\Users\" + Environment.UserName + @"\Documents";
+        if (!Directory.Exists(Environment.CurrentDirectory + @"\BlackRock"))
+        {
+            FileHandler.CreateRoot();
+        }
+        Environment.CurrentDirectory += @"\BlackRock";
     }
 }
